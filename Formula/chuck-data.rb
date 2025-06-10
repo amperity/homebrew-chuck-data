5b367c03a9855051ceea21e355d244ddab56c3a0
John Rush <john.rush@amperity.com>
2025-06-10 13:05:39 -0700

    update to use pypi

--- Formula/chuck-data.rb
+++ Formula/chuck-data.rb
@@ -3,7 +3,8 @@ class ChuckData < Formula
 
   desc "AI-powered customer data engineering assistant for Databricks"
   homepage "https://github.com/amperity/chuck-data"
-  url "https://files.pythonhosted.org/packages/f8/45/62bbfec660130c4b802bf65a879a1e637ef67364bcc39f2e619d1a55dafb/chuck_data-0.1.2.tar.gz"
+  version "0.1.2"
+  url "https://files.pythonhosted.org/packages/source/c/chuck-data/chuck_data-#{version}.tar.gz"
   sha256 "bcf32f0afc5aac30b6bc8245133c8b83e34df228290f4e48e02d6d1f256df13b"
   license "Apache-2.0"
 
@@ -11,7 +12,7 @@ class ChuckData < Formula
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.12/site-packages"
-    system "python3.12", "-m", "pip", "install", "--target=#{libexec}/lib/python3.12/site-packages", "chuck-data==0.1.2"
+    system "python3.12", "-m", "pip", "install", "--target=#{libexec}/lib/python3.12/site-packages", "chuck-data==#{version}"
     
     # Create wrapper script
     (bin/"chuck").write <<~EOS
