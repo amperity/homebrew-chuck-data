19712e04f350553bf6651efc7233cb6818079a34
john-b-rush <john.rush@amperity.com>
2026-01-12 03:46:47 -0800

    Update chuck-data formula to 0.3.2 (#6)
    Automated formula update for version 0.3.2
    
    Co-authored-by: john-b-rush <john-b-rush@users.noreply.github.com>

--- Formula/chuck-data.rb
+++ Formula/chuck-data.rb
@@ -3,9 +3,9 @@ class ChuckData < Formula
 
   desc "AI-powered customer data engineering assistant for Databricks"
   homepage "https://github.com/amperity/chuck-data"
-  version "0.3.1"
+  version "0.3.2"
   url "https://files.pythonhosted.org/packages/source/c/chuck-data/chuck_data-#{version}.tar.gz"
-  sha256 "fd88929604edcc52b300489951309efab28da1e45b40a6dd90e2774175710866"
+  sha256 "10f66ac3159cb84f4e1816c51b341baeb963a9b4832a2625e04245460b016537"
   license "Apache-2.0"
 
   depends_on "python@3.12"
