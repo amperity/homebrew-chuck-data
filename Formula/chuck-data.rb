274f2da34a948642b7de75320bfe080938c215ef
john-b-rush <john-b-rush@users.noreply.github.com>
2026-02-13 15:13:33 +0000

    Bump chuck-data to 0.4.5

--- Formula/chuck-data.rb
+++ Formula/chuck-data.rb
@@ -3,9 +3,9 @@ class ChuckData < Formula
 
   desc "AI-powered customer data engineering assistant for Databricks"
   homepage "https://github.com/amperity/chuck-data"
-  version "0.4.4"
+  version "0.4.5"
   url "https://files.pythonhosted.org/packages/source/c/chuck-data/chuck_data-#{version}.tar.gz"
-  sha256 "bbcd0869a174909a0b1cc76a4cf6f7d890abcf57a347abcefd93882cab913aa8"
+  sha256 "0caa30baaf82f70d0ecbc25e03fb2931cacc17c202820d4281b2ce0e65d8d743"
   license "Apache-2.0"
 
   depends_on "python@3.12"
